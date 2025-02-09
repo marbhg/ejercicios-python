85809aa53ea19dd9080a96c45d15130c3a492eff
Marrbe <132088541+marbhg@users.noreply.github.com>
2025-02-09 19:51:58 +0100

    Act
    Act

---
+++ marejercicios/2variables.py
@@ -0,0 +1,21 @@
+#Cadenas
+nombre = "Marbelis"
+
+#Numero (in)
+numero = 5
+
+#Numero con decimales  (float)
+pi = 3.15
+
+#Verdadero o falso sin comillas (True  o Flase) 
+gusta_python = true
+
+#Imprimir variables
+print("Nombre", Marbelis)
+print("Numero", numero)
+print("Pi", pi)
+print("Me gusta Python?", gusta_python)
+
+
+
+
